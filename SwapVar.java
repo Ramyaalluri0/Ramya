33a18608d32cd5bca20c153563236e7b1fc8fd6d
Ramyaalluri0 <ramyaalluri209@gmail.com>
2024-07-30 12:03:19 +0530

    swapping two variables using temp

---
+++ SwapVar.java
@@ -0,0 +1,12 @@
+class SwapVar{
+public static void main(String[] args){
+String a="sana";
+String b="hana";
+String temp;
+temp=a;
+a=b;
+b=temp;
+System.out.println("a: "+a);
+System.out.println("b: "+b);
+}
+}
